eeba108ba9e7c540268c51c6ec15da64cfa4b225
Tae-Su Kang <mtae616@naver.com>
2024-01-21 18:03:27 +0900

    webclient + web mvc

--- src/main/java/com/example/AsynApplication2.java
+++ src/main/java/com/example/AsynApplication2.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 // 비동기 RestTemplate 과 비동기 MVC 결합
 @SpringBootApplication
@@ -12,9 +15,23 @@ public class AsynApplication2 {
 
     @RestController
     public static class MyController {
+        RestTemplate rt = new RestTemplate();
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://localhost:8081")
+                .build();
+
         @GetMapping("/rest")
-        public String rest() {
-            return "rest";
+        // webclient 는 reactor 를사용
+        // visualvm 확인 결과 실제로 100개 를 req 하더라도 reactor http nio 는 8개 정도 사용
+        // reactor 는 core 의 1배수로 thread 를 사용
+        public Mono<String> rest(int idx) {
+            return webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/service")
+                            .queryParam("req", "hello" + idx)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class);
         }
     }
 
--- src/main/java/com/example/LoadTest.java
+++ src/main/java/com/example/LoadTest.java
@@ -4,34 +4,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StopWatch;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class LoadTest {
     static AtomicInteger counter = new AtomicInteger(0);
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
         ExecutorService es = Executors.newFixedThreadPool(100);
         RestTemplate rt = new RestTemplate();
-        String url = "http://localhost:8080/callable";
+        String url = "http://localhost:8080/rest?idx={idx}";
+
+        CyclicBarrier barrier = new CyclicBarrier(101);
+
         StopWatch main = new StopWatch();
         main.start();
 
         for (int i = 0; i < 100; i++) {
-          es.execute(() -> {
-              int idx = counter.addAndGet(1);
-              log.info("Thread {}", idx);
-              StopWatch sw = new StopWatch();
-              sw.start();
-              rt.getForObject(url, String.class);
-              sw.stop();
-              log.info("Elapsed: {} {}", idx, sw.getTotalTimeSeconds());
-          });
+            es.submit(() -> {
+                int idx = counter.addAndGet(1);
+
+                barrier.await(); // thread 가 전부 준비될 때까지 block
+
+                log.info("Thread {}", idx);
+
+                StopWatch sw = new StopWatch();
+                sw.start();
+
+                String res = rt.getForObject(url, String.class, idx);
+
+                sw.stop();
+                log.info("Elapsed: {} {} / {}", idx, sw.getTotalTimeSeconds(),res);
+                return null;
+            });
+
         }
 
+        barrier.await();
         es.shutdown();
         es.awaitTermination(100, TimeUnit.SECONDS);
 
